d2943fad7921953f0663a9262f23239e3cc69915
xuweicn1 <821382128@qq.com>
2018-10-16 12:07:10 +0800

    task03-v1.2

---
+++ task03_hx711-pt100_temperature-measurement_v1.2/measure_t.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+from driverhx711 import HX711
+from driverhx711 import Thermometer
+from driverhx711 import ResMeasure
+import time
+
+
+while True:
+    cont=HX711(4,17).getdv()
+    c= float(cont)/100
+    r=ResMeasure().get_res(c)
+    t = Thermometer().read(r)
+    print('当前电阻是：','%.2f' % r,'Ω')
+    print('当前温度为：','%.2f' % t,'℃')
+    print('*********************')
+    time.sleep(5)
+
---
+++ task03_hx711-pt100_temperature-measurement_v1.2/readme.txt
@@ -0,0 +1,3 @@
+1.将measure_t.py 、driverhx711.py 上传到树莓派同一个目录；
+2.连接好电路
+3.树莓派命令：Python3 measure_t.py
